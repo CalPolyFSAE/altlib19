c5d28dc6ba0e12a4035c7e054f05f28a1ad895e1
Baylor Whitehead <bwhitehe@calpoly.edu>
2019-10-16 22:14:35 -0700

    fixed diode

--- CPFAltiumUI/src/DiodeAddEditPanel.java
+++ CPFAltiumUI/src/DiodeAddEditPanel.java
@@ -1,12 +1,8 @@
 import javax.swing.*;
 
 public class DiodeAddEditPanel extends BaseAddEditPanel{
-    protected BaseAddEditPanel.LabelAndInput current  = new BaseAddEditPanel.LabelAndInput(new JLabel("Current(A)*:"), new JTextArea(1, 15));
     protected BaseAddEditPanel.LabelAndInput voltage = new BaseAddEditPanel.LabelAndInput(new JLabel("Voltage(V)*:"), new JTextArea(1, 15));
-    protected BaseAddEditPanel.LabelAndInput type = new BaseAddEditPanel.LabelAndInput(new JLabel("Type*:"), new JComboBox(new String[]{"Gunn", "Laser", "Varactor", "Schottky", "Zener", "Tunnel", "PIN", "Photo", "Step Recovery"}));
     protected BaseAddEditPanel.LabelAndInput numcircuits = new BaseAddEditPanel.LabelAndInput(new JLabel("Number Circuits*:"), new JTextArea(1, 15));
-    protected BaseAddEditPanel.LabelAndInput ic = new BaseAddEditPanel.LabelAndInput(new JLabel("IC(A)*:"), new JTextArea(1, 15));
-    protected BaseAddEditPanel.LabelAndInput power = new BaseAddEditPanel.LabelAndInput(new JLabel("Power(W)*:"), new JTextArea(1, 15));
     protected BaseAddEditPanel.LabelAndInput packages = new BaseAddEditPanel.LabelAndInput(new JLabel("Package:"), new JTextArea(1, 15));
 
     public DiodeAddEditPanel(boolean isadd){
@@ -18,12 +14,8 @@ public class DiodeAddEditPanel extends BaseAddEditPanel{
     {
         addBasicInput();
 
-        addLabelAndInput(current);
         addLabelAndInput(voltage);
-        addLabelAndInput(type);
         addLabelAndInput(numcircuits);
-        addLabelAndInput(ic);
-        addLabelAndInput(power);
         addLabelAndInput(packages);
 
         addLibraryFootprintInput();
--- CPFAltiumUI/src/DiodeDao.java
+++ CPFAltiumUI/src/DiodeDao.java
@@ -21,25 +21,20 @@ public class DiodeDao {
         PreparedStatement ps = null;
         try {
             ps = this.connection.prepareStatement(
-                    "INSERT INTO `cpf_altium`.`diodes` (`id`, `name`, `current`, `voltage`, `type`, `numcircuits`, `ic`, `power`, `package`, `quantity`, `location`, `Description`, `Library Ref`, `Library Path`, `Footprint Ref`, `Footprint Path`, `createdby`, `createddate`, `updatedby`, `updateddate`)"+
-                            " VALUES (0, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, NOW(), ?, NOW())");
+                    "INSERT INTO `cpf_altium`.`diodes` (`id`, `name`, `vr`, `numcircuits`, `package`, `quantity`, `Description`, `Library Ref`, `Library Path`, `Footprint Ref`, `Footprint Path`, `createdby`, `createddate`, `updatedby`, `updateddate`)"+
+                            " VALUES (0, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, NOW(), ?, NOW())");
             ps.setString(1, diode.getName());
-            ps.setFloat(2, diode.getCurrent());
-            ps.setFloat(3, diode.getVoltage());
-            ps.setString(4, diode.getType());
-            ps.setInt(5, diode.getNumcircuits());
-            ps.setFloat(6, diode.getIc());
-            ps.setFloat(7, diode.getPower());
-            ps.setString(8, diode.getPackageString());
-            ps.setInt(9, diode.getQuantity());
-            //ps.setString(10, diode.getLocation());
-            ps.setString(11, diode.getDescription());
-            ps.setString(12, diode.getLibraryRef());
-            ps.setString(13, diode.getLibraryPath());
-            ps.setString(14, diode.getFootprintRef());
-            ps.setString(15, diode.getFootprintPath());
-            ps.setString(16, DefaultData.name);
-            ps.setString(17, DefaultData.name);
+            ps.setFloat(2, diode.getVoltage());
+            ps.setInt(3, diode.getNumcircuits());
+            ps.setString(4, diode.getPackageString());
+            ps.setInt(5, diode.getQuantity());
+            ps.setString(6, diode.getDescription());
+            ps.setString(7, diode.getLibraryRef());
+            ps.setString(8, diode.getLibraryPath());
+            ps.setString(9, diode.getFootprintRef());
+            ps.setString(10, diode.getFootprintPath());
+            ps.setString(11, DefaultData.name);
+            ps.setString(12, DefaultData.name);
 
             ret = ps.executeUpdate();
         } catch (SQLException e) {
